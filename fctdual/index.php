7630d921292fb9e9043ccc0f099c65c9c7c972ee
javier <javi.bielsa1995@gmail.com>
2017-05-15 11:15:08 +0200

    fctdual

---
+++ fctdual/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<HTML>
+<HEAD>
+<TITLE> index.php </TITLE>
+<META NAME="Generator" CONTENT="EditPlus">
+<META NAME="Author" CONTENT="">
+<META NAME="Keywords" CONTENT="">
+<META NAME="Description" CONTENT="">
+</HEAD>
+
+<BODY>
+<?php
+include("conexion.php");
+?>
+<form name="usuarios" id="usuarios" method="post" action="login.php">
+<table align="center" width="40%">
+<tr>
+	<td colspan="2"><img src="./imagenes/logosz.jpg" width="500"></td>
+</tr>
+<tr>
+	<td>Dni: </td>
+	<td><input type="text" name="dni" id="dni" placeholder="dni">
+</tr>
+<tr>
+	<td>Clave:</td>
+	<td><input type="password" name="clave" id="clave" placeholder="Clave"></td>
+</tr>
+<tr>
+	<td colspan="2" align="center">
+		<input type="submit" value="Entrar">
+	</td>
+</tr>
+</table>
+</form>
+</BODY>
+</HTML>
---
+++ fctdual/login.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+// Recuperar datos del formulario
+$dni=$_POST['dni'];
+$cla=$_POST['clave'];
+
+include("conexion.php");
+
+//Localizar el dni en la tabla usuarios
+$sql="SELECT u.*, e.nif FROM usuarios as u, empresas as e WHERE u.dni=e.dniusuario and u.dni='$dni'";
+
+$registros=mysqli_query($conexion,$sql);
+$total=mysqli_num_rows($registros);
+if($total==0)
+{
+	echo "<H2>USUARIO NO EXISTE </H2>Pulse <a href='index.php'>aquí</a> para continuar.";
+}
+else
+{
+	while($linea=mysqli_fetch_array($registros))
+	{
+		if($linea['clave']==$cla)
+		{
+			$_SESSION['dniusuario']=$linea['dni'];
+			$_SESSION['nombreusuario']=$linea['nombre'];
+			if($linea['nif']!='R5000178J')
+				header("location:forminformes.php");
+			else
+				header("location:gestion.php");
+		}
+		else
+		{
+			echo "<H2>CLAVE INCORRECTA </H2>Pulse <a href='index.php'>aquí</a> para continuar.";
+		}
+
+	}
+}
+
+
+?>
